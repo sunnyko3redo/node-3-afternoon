688af02e7913dcc92b294eb0affe1f2b271ad653
sunnyko3 <57108786+sunnyko3@users.noreply.github.com>
2019-12-18 16:37:31 -0700

    turnin

---
+++ db/create_product.sql
@@ -0,0 +1,6 @@
+insert into product (name, description, price, image_url)
+values ($1,$2, $3, $4);
+
+insert into product (name, description, price, image_url) values ($1, $2, $3, $4)
+
+returning *;
